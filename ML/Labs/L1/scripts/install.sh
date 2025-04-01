cdf30c2588f7a25a4d1072e2348d16325526bf28
nickAnhel <anhimovn1@gmail.com>
2025-04-01 14:30:10 +0300

    Fix L1 (ML)

--- ML/Labs/L1/scripts/install.sh
+++ ML/Labs/L1/scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -L -o ~/Study/S6/ML/L1/fer2013.zip https://www.kaggle.com/api/v1/datasets/download/msambare/fer2013
+curl -L -o ~/Study/S6/ML/Labs/L1/fer2013.zip https://www.kaggle.com/api/v1/datasets/download/msambare/fer2013
 unzip fer2013.zip -d ../dataset/
 rm fer2013.zip
