2412daa7ccd610db6ef271403a9a0d7f1795bb99
nickAnhel <anhimovn1@gmail.com>
2025-04-11 01:00:21 +0300

    L5 (ML)

---
+++ ML/Labs/L5/scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -L -o ~/Study/S6/ML/Labs/L4/online-retail-ii-uci.zip https://www.kaggle.com/api/v1/datasets/download/mashlyn/online-retail-ii-uci
+unzip online-retail-ii-uci.zip -d  ~/Study/S6/ML/Labs/L4/dataset/
+rm online-retail-ii-uci.zip`
